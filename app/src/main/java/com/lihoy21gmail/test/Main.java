ce675fb0bda384e0637ef97636c553062e1c61ea
Потёмкин <lihoy21@gmail.ru>
2015-09-16 15:59:20 +0300

    Начальный commit

---
+++ app/src/main/java/com/lihoy21gmail/test/Main.java
@@ -0,0 +1,19 @@
+package com.lihoy21gmail.test;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class Main extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MainFragment myFrag = new MainFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.cont, myFrag).commit();
+    }
+
+
+}
